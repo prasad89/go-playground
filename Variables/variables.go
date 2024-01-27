c80de482dbaae1143b30f8d44168b131318b33a9
Prasad Bhalerao <67261499+prasad89@users.noreply.github.com>
2024-01-27 13:16:46 +0000

    Variables

---
+++ Variables/variables.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// explicitly declare the data type
+	var number1 int = 1
+	fmt.Println(number1)
+
+	// assign a value without declaring the data type
+	number2 := 2
+	fmt.Println(number2)
+
+	// shorthand notation to define variable
+	number3 := 3
+	fmt.Println(number3)
+}
