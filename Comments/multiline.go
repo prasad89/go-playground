9d453b62380e0b20a5967d69bab1965086d665bb
Prasad Bhalerao <67261499+prasad89@users.noreply.github.com>
2024-02-04 15:06:37 +0000

    Comments

---
+++ Comments/multiline.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	/* creates a variable
+	to store the salary of the employee
+	*/
+
+	salary := 30000
+	fmt.Println("Salary:", salary)
+}
---
+++ Comments/singleline.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// declare a variable
+
+	age := 23
+
+	// print the variable
+	fmt.Println("Age is", age)
+}
