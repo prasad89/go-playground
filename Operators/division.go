f7a12885d14d809e9b67cc87d30be6f606364d12
Prasad Bhalerao <67261499+prasad89@users.noreply.github.com>
2024-02-08 10:26:51 +0000

    Type Casting

---
+++ Operators/division.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// num1 := 11
+	// num2 := 4
+
+	// // divide two integer variables
+	// quotient := num1 / num2
+	// fmt.Printf("%d / %d = %d\n", num1, num2, quotient)
+
+	num1 := 11.0
+	num2 := 4.0
+
+	// divide two floating point variables
+	result := num1 / num2
+	fmt.Printf("%g / %g = %g\n", num1, num2, result)
+}
---
+++ Operators/inc_dec.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	num := 5
+
+	// increment of num by 1
+	num++
+	fmt.Println(num)
+
+	// decrement of num by 1
+	num--
+	fmt.Println(num)
+}
